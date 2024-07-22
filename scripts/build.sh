54a611afc6ae9c49f2679cb8739e1bdbcd96bf81
Gervasio Junior <jamilservicos@gmail.com>
2024-07-22 19:35:55 -0300

    Create build.sh

---
+++ scripts/build.sh
@@ -0,0 +1,23 @@
+#!/bin/env bash
+
+FIREWALLD_VERSION=v2.2.0
+
+EXTERNALS_SOURCE_FOLDER=/opt/owle-externals/source
+EXTERNALS_BUILD_FOLDER=/opt/owle-externals/build
+
+apt update
+apt install gettext intltool xmlto git
+
+mkdir -p $EXTERNALS_SOURCE_FOLDER
+mkdir -p $EXTERNALS_BUILD_FOLDER
+
+# FirewallD ${FIREWALLD_VERSION}
+git clone https://github.com/firewalld/firewalld.git $EXTERNALS_SOURCE_FOLDER/firewalld
+cd $EXTERNALS_SOURCE_FOLDER/firewalld
+git checkout ${FIREWALLD_VERSION}
+./autogen.sh
+./configure --prefix=$EXTERNALS_BUILD_FOLDER
+make
+make install
+
+exit 0
