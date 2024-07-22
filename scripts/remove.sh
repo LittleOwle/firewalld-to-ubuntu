60555d3d4bf4fe54dbe91d05b3ce4e83aacd70d9
Gervasio Junior <jamilservicos@gmail.com>
2024-07-22 19:40:27 -0300

    Create remove.sh

---
+++ scripts/remove.sh
@@ -0,0 +1,13 @@
+#!/bin/env bash
+
+FIREWALLD_VERSION=v2.2.0
+
+EXTERNALS_SOURCE_FOLDER=/opt/owle-externals/source/firewalld
+EXTERNALS_BUILD_FOLDER=/opt/owle-externals/build/firewalld
+
+rm -rf $EXTERNALS_SOURCE_FOLDER
+rm -rf $EXTERNALS_BUILD_FOLDER
+
+echo "remove done!"
+
+exit 0
