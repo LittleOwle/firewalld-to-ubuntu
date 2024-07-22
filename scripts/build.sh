13d0c2eecb0076c56962a5b568da8a9513a2b28c
Gervasio Junior <jamilservicos@gmail.com>
2024-07-22 19:38:50 -0300

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,8 +2,8 @@
 
 FIREWALLD_VERSION=v2.2.0
 
-EXTERNALS_SOURCE_FOLDER=/opt/owle-externals/source
-EXTERNALS_BUILD_FOLDER=/opt/owle-externals/build
+EXTERNALS_SOURCE_FOLDER=/opt/owle-externals/source/firewalld
+EXTERNALS_BUILD_FOLDER=/opt/owle-externals/build/firewalld
 
 apt update
 apt install gettext intltool xmlto git
@@ -12,8 +12,8 @@ mkdir -p $EXTERNALS_SOURCE_FOLDER
 mkdir -p $EXTERNALS_BUILD_FOLDER
 
 # FirewallD ${FIREWALLD_VERSION}
-git clone https://github.com/firewalld/firewalld.git $EXTERNALS_SOURCE_FOLDER/firewalld
-cd $EXTERNALS_SOURCE_FOLDER/firewalld
+git clone https://github.com/firewalld/firewalld.git $EXTERNALS_SOURCE_FOLDER
+cd $EXTERNALS_SOURCE_FOLDER
 git checkout ${FIREWALLD_VERSION}
 ./autogen.sh
 ./configure --prefix=$EXTERNALS_BUILD_FOLDER
