4495abd219ee0215f3ccb86943f11e6aababa5b1
Gervasio Junior <jamilservicos@gmail.com>
2024-07-22 20:06:26 -0300

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,7 +2,7 @@
 
 FIREWALLD_VERSION=v2.2.0
 
-EXTERNALS_SOURCE_FOLDER=/opt/owle-externals/source/firewalld
+EXTERNALS_SOURCE_FOLDER=/opt/owle-externals/source/firewalld-${FIREWALLD_VERSION}
 EXTERNALS_BUILD_FOLDER=/opt/owle-externals/build/firewalld
 
 apt update
@@ -15,6 +15,8 @@ mkdir -p $EXTERNALS_BUILD_FOLDER
 git clone https://github.com/firewalld/firewalld.git $EXTERNALS_SOURCE_FOLDER
 cd $EXTERNALS_SOURCE_FOLDER
 git checkout ${FIREWALLD_VERSION}
+export FIREWALLD_DEVEL_ENV=$(pwd)/src
+export PYTHONPATH=$FIREWALLD_DEVEL_ENV
 ./autogen.sh
 ./configure --prefix=$EXTERNALS_BUILD_FOLDER
 make
