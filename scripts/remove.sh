b6907af8eb9879e13b8cfa25756c70f39dfb6272
Gervasio Junior <jamilservicos@gmail.com>
2024-07-22 20:06:45 -0300

    Update remove.sh

--- scripts/remove.sh
+++ scripts/remove.sh
@@ -2,7 +2,7 @@
 
 FIREWALLD_VERSION=v2.2.0
 
-EXTERNALS_SOURCE_FOLDER=/opt/owle-externals/source/firewalld
+EXTERNALS_SOURCE_FOLDER=/opt/owle-externals/source/firewalld-${FIREWALLD_VERSION}
 EXTERNALS_BUILD_FOLDER=/opt/owle-externals/build/firewalld
 
 rm -rf $EXTERNALS_SOURCE_FOLDER
